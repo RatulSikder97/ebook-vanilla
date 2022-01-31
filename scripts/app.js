bb75c36a3f3e08facc3b7d7cdbed754cc5a03d32
Ratul <ratul.sikder@rokomari.com>
2022-01-31 15:45:47 +0600

    Fix conflict

--- scripts/app.js
+++ scripts/app.js
@@ -149,11 +149,7 @@ function checkBookUrl() {
  * Initiate Book
  */
 function initBook() {
-<<<<<<< HEAD
 	book = ePub("/assets/book/Crime-and-Punishment-Fyodor-Dostoevsky.epub");
-=======
-	book = ePub(BOOK_ID);
->>>>>>> 0484bc0abff7bcec652bdaf5ae0442e5bb915a61
 }
 
 /**
@@ -360,11 +356,7 @@ function getTotalPage(fontSize) {
 	container.appendChild(hiddenEpubElement);
 	document.body.appendChild(container);
 
-<<<<<<< HEAD
 	const hiddenBook = ePub("/assets/book/Crime-and-Punishment-Fyodor-Dostoevsky.epub");
-=======
-	const hiddenBook = ePub(BOOK_ID);
->>>>>>> 0484bc0abff7bcec652bdaf5ae0442e5bb915a61
 
 	hiddenRender = hiddenBook.renderTo(hiddenEpubElement, BOOK_RENDER_OPTION);
 	hiddenRender.themes.fontSize(fontSize+'px')
